52d474250750237274f7f60aae41f7e61821269e
NiksenB <90034452+NiksenB@users.noreply.github.com>
2022-04-04 20:03:10 +0200

    Add a bunch of updates from Jesperman

--- ScrabbleBot/Parser.fs
+++ ScrabbleBot/Parser.fs
@@ -5,6 +5,7 @@
 
 module internal Parser
 
+    open StateMonad
     open ScrabbleUtil // NEW. KEEP THIS LINE.
     open System
     open Eval
@@ -23,7 +24,6 @@ module internal Parser
     let pFalse      = pstring "false"
     let pIsDigit    = pstring "isDigit"
     let pIsLetter   = pstring "isLetter"
-    let pIsVowel   = pstring "isVowel"
 
     let pif       = pstring "if"
     let pthen     = pstring "then"
@@ -32,7 +32,7 @@ module internal Parser
     let pdo       = pstring "do"
     let pdeclare  = pstring "declare"
 
-    let  whitespaceChar = satisfy System.Char.IsWhiteSpace <?> "whitespace"
+    let whitespaceChar = satisfy System.Char.IsWhiteSpace <?> "whitespace"
     let pletter        = satisfy System.Char.IsLetter <?> "letter"
     let palphanumeric  = satisfy System.Char.IsLetterOrDigit <?> "alphanumeric"
 
@@ -59,43 +59,105 @@ module internal Parser
     let binop op p1 p2  =
         (p1 .>*>. op ) .>*>. p2 |>> fun ((x,y),z) -> (x,z) 
 
-    let TermParse, tref = createParserForwardedToRef<aExp>()
+    //Arith (and char) parse
+    let TermParse, tref = createParserForwardedToRef<aExp>() //loosest
     let ProdParse, pref = createParserForwardedToRef<aExp>()
-    let AtomParse, aref = createParserForwardedToRef<aExp>()
+    let AtomParse, aref = createParserForwardedToRef<aExp>() //hardest
+
+    let CParse, cref = createParserForwardedToRef<cExp>()
 
     let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
-    do tref := choice [AddParse; ProdParse]
+    let SubParse = binop (pchar '-') ProdParse TermParse |>> Sub <?> "Sub"
 
     let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
-    do pref := choice [MulParse; AtomParse]
+    let DivParse = binop (pchar '/') AtomParse ProdParse |>> Div <?> "Div"
+    let ModParse = binop (pchar '%') AtomParse ProdParse |>> Mod <?> "Mod"
 
     let NParse   = pint32 |>> N <?> "Int"
-    let ParParse = parenthesise TermParse
-    do aref := choice [NParse; ParParse]
+    let NegParse = unop (pchar '-') AtomParse |>> (fun x -> Mul ((N -1), x)) <?> "Neg"
+    //let NegParse = (unop (pchar '-') pint32 |>> (fun y -> y * (-1))) |>> N <?> "Neg"
+    let PVParse   = unop pPointValue AtomParse |>> PV <?> "PV"
+    let IdParse   = pid |>> V <?> "Variable"
+    let ParParse = parenthesise TermParse <?> "Par"
+    let CharToIntParse = pCharToInt  >*>. (parenthesise CParse) |>> CharToInt <?> "CharToInt"
+
+    do tref := choice [AddParse; SubParse; ProdParse]
+    do pref := choice [MulParse; ModParse; DivParse; AtomParse]
+    do aref := choice [NegParse; PVParse; NParse; ParParse; CharToIntParse; IdParse]
 
     let AexpParse = TermParse 
 
-    let CexpParse = pstring "not implemented"
+    //Char parse
+    let CharParse =   pchar '\'' >>. anyChar .>> pchar '\'' |>> C <?> "Char" 
+    let CharValParse = (>*>.) pCharValue ParParse |>> CV <?> "CV"
+    let IntToCharParse = pIntToChar >*>. ParParse |>> IntToChar <?> "IntToChar"
+    let ToLowerParse = pToLower >*>. (parenthesise CParse) |>> ToLower <?> "ToLower"
+    let ToUpperParse = pToUpper >*>. (parenthesise CParse) |>> ToUpper <?> "ToUpper"
 
-    let BexpParse = pstring "not implemented"
+    do cref := choice [CharParse; CharValParse; ToUpperParse; ToLowerParse; IntToCharParse]
 
-    let stmParse = pstring "not implemented"
+    let CexpParse = CParse
+
+    //Boolean parse
+    let JunctionBoolParse, jref = createParserForwardedToRef<bExp>() //loosest
+    let ArithBoolParse, abref = createParserForwardedToRef<bExp>()
+    let ValidateBoolParse, vref = createParserForwardedToRef<bExp>() //hardest
+
+    let ConjParse = ArithBoolParse .>*> pstring @"/\" .>*>. JunctionBoolParse |>> Conj <?> "Conj" 
+    let DisjParse = ArithBoolParse .>*> pstring @"\/" .>*>. JunctionBoolParse |>> (fun (x,y) -> Not (Conj (Not (x), Not (y)))) <?> "Disj" 
+    do jref := choice [ConjParse; DisjParse; ArithBoolParse]
+
+    let EqParse = AtomParse .>*> pchar '=' .>*>. AtomParse  |>> AEq <?> "AEq"
+    let UnEqParse = AtomParse .>*> pstring "<>" .>*>. AtomParse |>> (fun (x,y) -> Not (AEq (x,y))) <?> "AUnEq" 
+    let LtParse = AtomParse .>*> pchar '<' .>*>. AtomParse  |>> ALt <?> "ALt"
+    let LtoEtParse =  TermParse .>*> pstring "<=" .>*>. TermParse |>> (fun (x, y) -> Not (Conj (Not (ALt (x, y)), Not (Not (Not (AEq (x, y))))))) <?> "ALtoEt"
+    let GtParse = TermParse .>*> pchar '>' .>*>. TermParse |>> (fun (x, y) ->  (Conj (Not (AEq (x,y)), Not (ALt (x,y))))) <?> "AGt"
+    let GtoEtParse =  TermParse .>*> pstring ">=" .>*>. TermParse |>> (fun (x,y) -> Not (ALt (x,y))) <?> "AGt"
+    do abref := choice [LtParse; LtoEtParse; GtParse; GtoEtParse; EqParse; UnEqParse; ValidateBoolParse]
+
+    let TParse = pTrue |>> (fun _ -> TT) <?> "True"
+    let FParse = pFalse |>> (fun _ -> FF) <?> "False"
+    let IsLetterParse = unop pIsLetter (parenthesise CParse) |>> IsLetter <?> "IsLetter"
+    let IsVowelParse = unop pIsVowel (parenthesise CParse) |>> IsVowel <?> "IsVowel"
+    let IsDigitParse = unop pIsDigit (parenthesise CParse) |>> IsDigit <?> "IsDigit"
+    let NotParse = pchar '~' >*>. JunctionBoolParse |>> Not <?> "Not"
+    let ParBoolParse = parenthesise JunctionBoolParse <?> "ParBool"
+
+    do vref := choice [TParse; FParse; IsLetterParse; IsLetterParse; IsVowelParse; IsDigitParse; NotParse; ParBoolParse]
+
+    let BexpParse = JunctionBoolParse
+
+    //Statement parse
+    let SequenceParse, ssref = createParserForwardedToRef<stm>()
+    let StatementParse, sref = createParserForwardedToRef<stm>()
+
+    let SeqParse =  StatementParse .>*> pchar ';' .>*>. SequenceParse |>> Seq <?> "Seq"
+    do ssref := choice [SeqParse; StatementParse]
+
+    let AssParse = pid .>*> pstring ":=" .>*>. AexpParse |>> Ass <?> "Ass"
+    let DeclareParse = pdeclare >>. spaces1 >*>. pid |>> Declare <?> "Declare"
+    let ITEParse = pif >*>. ParBoolParse .>*> pthen .>*>. curlyBrackets SequenceParse .>*> pelse .>*>. curlyBrackets SequenceParse |>> (fun ((x,y),z) -> ITE(x, y, z)) <?> "ITE"
+    let ITParse = pif >*>. ParBoolParse .>*> pthen .>*>. curlyBrackets SequenceParse |>> (fun (x,y) -> ITE (x, y, Skip)) <?> "IT"
+    let WhileParse =  pwhile .>> spaces1 >>. ParBoolParse .>> spaces1 .>> pdo .>> spaces1 .>>. curlyBrackets SequenceParse |>> While <?> "While"
+    
+    do sref := choice [AssParse; DeclareParse; WhileParse; ITEParse; ITParse]
+
+    let stmParse = SequenceParse
+
 
     (* The rest of your parser goes here *)
 
     type word   = (char * int) list
-    type square = Map<int, word -> int -> int -> int>
+    type squareFun = word -> int -> int -> Result<int, Error>
+    type square = Map<int, squareFun>
 
-    let parseSquareFun _ = failwith "not implemented"
+    type boardFun2 = coord -> Result<square option, Error>
 
-    let parseBoardFun _ = failwith "not implemented"
-
-    type boardFun = coord -> square option
     type board = {
         center        : coord
         defaultSquare : square
-        squares       : boardFun
+        squares       : boardFun2
     }
 
-    let parseBoardProg (bp : boardProg) : board = failwith "not implemented"
-
+    // Default (unusable) board in case you are not implementing a parser for the DSL.
+    let mkBoard : boardProg -> board = fun _ -> {center = (0,0); defaultSquare = Map.empty; squares = fun _ -> Success (Some Map.empty)}
