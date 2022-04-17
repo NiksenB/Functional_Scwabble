29a9a854c9e6dcbf00db1908b5c0e796ff9074c7
Mai_Sigurd <odgaardmai@gmail.com>
2022-04-17 18:14:22 +0200

    Yellow assigments from 6

--- ScrabbleBot/Eval.fs
+++ ScrabbleBot/Eval.fs
@@ -120,21 +120,21 @@
     | ITE of bExp * stm * stm (* if-then-else statement *)
     | While of bExp * stm     (* while statement *)
 
-    //LOOK HERE, WE NEED THIS. MAI MAYBE HAS BETTER VERSION? MAY ALSO BE CALLED "EvalStm" or something...?
-    let rec stmntEval (stmnt : stm) : SM<unit> = 
-        match stmnt with 
-        //| Declare -> 
-        // | Ass (var, v) -> (let val = arithEval V
-        //                     )
-        | Skip -> ret ()
-        // | Seq
-        // | ITE 
-        // | While 
-
+   
+    let rec stmntEval stmnt : SM<unit> =
+        match stmnt with
+        |Skip -> ret ()
+        |Ass (x,a) -> 
+            arithEval a >>= (update x)   
+        |Seq (stm1, stm2) -> stmntEval stm1 >>>=  stmntEval stm2 
+        |ITE (guard, stm1, stm2) ->
+            (boolEval guard)  >>= (fun x  -> push >>>= if x then pop >>>= (stmntEval stm1) else pop >>>= stmntEval stm2)            
+        |While (guard, stm) ->
+            (boolEval guard)  >>= (fun b  -> push >>>= if b then pop >>>= stmntEval stm >>>= stmntEval (While (guard, stm))  else  pop >>>= ret ())
+        |Declare x -> declare x 
 
 
 (* Part 3 (Optional) *)
-
     type StateBuilder() =
 
         member this.Bind(f, x)    = f >>= x
@@ -145,36 +145,155 @@
         
     let prog = new StateBuilder()
 
-    let arithEval2 a = failwith "Not implemented"
-    let charEval2 c = failwith "Not implemented"
-    let rec boolEval2 b = failwith "Not implemented"
+    let arithEval2 a =
+        prog{
+        match a with
+            | N n -> return n
+            | V v -> return!  lookup v
+            | PV x ->  
+                        let! p = arithEval x
+                        return! (pointValue p)
+            | WL ->  return! wordLength
+            | Add (a, b) -> return!  add (arithEval a) (arithEval b) 
+            | Sub (a, b) -> return!  sub (arithEval a) (arithEval b)  
+            | Mul (a, b) -> return!  mul (arithEval a) (arithEval b) 
+            | Div (a, b) -> return! (div (arithEval a) (arithEval b))
+            | Mod (a,b ) -> 
+                        let! x = arithEval a
+                        let! y = arithEval b
+                        if y <> 0 then return (x % y) else return! fail DivisionByZero                
+            | CharToInt ch ->
+                    let! x = charEval ch
+                    return System.Char.ToUpper x |> (int)
+            }
+    let charEval2 c =
+        prog{
+            match c with
+            |C ch -> return ch
+            |ToUpper ch ->
+                let! x' = charEval ch
+                return (System.Char.ToUpper x')
+            |ToLower ch ->
+                let! x' = charEval ch
+                return (System.Char.ToLower x') 
+            |CV i ->
+                let! x = arithEval i
+                return! characterValue x
+            |IntToChar i' ->
+                let! i'' = arithEval i'
+                return char(i'')
+        }
+    let rec boolEval2 b =
+        prog{
+            match b with 
+            | TT           -> return true                   
+            | FF           -> return false        
+            | AEq (a,b)     ->
+                let! x = arithEval a
+                let! y = arithEval  b
+                return (x = y)
+            | ALt (a,b)     ->
+                let! x = arithEval a
+                let! y = arithEval  b
+                return (x > y)
+            | Not b         ->
+                let! b' = boolEval b
+                return (not b')            
+            | Conj (a,b)    ->
+                let! x = boolEval a
+                let! y = boolEval  b
+                return  (x && y)
+        }
 
-    let stmntEval2 stm = failwith "Not implemented"
+    let stmntEval2 stm =
+        prog{
+            match stm with
+            |Skip -> return ()
+            |Ass (x,a) -> 
+                let! a' = arithEval a
+                return! (update x a')   
+            |Seq (stm1, stm2) ->
+                do! stmntEval stm1
+                do! stmntEval stm2 
+            |ITE (guard, stm1, stm2) ->
+                let! g = boolEval guard
+                do! push
+                if g
+                then
+                    do! pop
+                    return! (stmntEval stm1)
+                else
+                    do! pop
+                    return! (stmntEval stm2)            
+            |While (guard, stm) ->
+                let! g = boolEval guard
+                do! push
+                if g
+                then
+                    do! pop
+                    do! stmntEval stm
+                    return! stmntEval (While (guard, stm))
+                else
+                    do! pop
+                    return ()
+            |Declare x -> return! (declare x )
+        }
 
 (* Part 4 (Optional) *)
+    
+    
 
     type word = (char * int) list
     type squareFun = word -> int -> int -> Result<int, Error>
-
+    
+    type coord = int * int
+    
+    type boardFun = coord -> Result<squareFun option, Error>
+    
     //LOOK HERE, WE NEED THIS. IT IS FROM 6.12 BUT BASED ON WORK WE DID IN 3.7
-    let stmntToSquareFun (stm : stm) : squareFun = fun mkState
+    
+    
+    //let stmntToSquareFun (stm : stm) : squareFun = fun mkState
+   
+    
+    let stmntToBoardFun (stm : stm) (m: Map<int, squareFun> ) : boardFun =
+        let sf c =
+            let vars = [("_x_", fst(c)); ("_y_",snd(c)); ("_result_",0)]
+            let set = [("_x_"); ("_y_"); ("_result_")]
+            let i = stmntEval2 stm >>>= lookup "_result_" |> evalSM ( mkState vars [] set )
+            match i with
+            | Success (a) ->  Success(m.TryFind a)
+            | Failure (a) -> Failure ( a)
+            
+        sf  
 
 
     //SHOULD THE TYPE DEFINITIONS AND FUNCTIONS BELOW BE HERE IN EVAL, OR IN PARSER.fs? SOME OF THE LINES ARE CURRENTLY IN BOTH PLACES
 
+    let stmntToSquareFun stm : squareFun= 
+        let sf word pos acc =
+                let vars = [("_pos_", pos); ("_acc_",acc); ("_result_",0)]
+                let set = [("_pos_"); ("_acc_"); ("_result_")]
+                stmntEval2 stm >>>= lookup "_result_" |> evalSM ( mkState vars  word set )           
+             
+        sf 
+
+    
+
+    
+
+    
+
+    type board = {
+        center        : coord
+        defaultSquare : squareFun
+        squares       : boardFun
+    }
 
 
-    // type coord = int * int
-
-    // type boardFun = coord -> Result<squareFun option, Error> 
+    
 
     // let stmntToBoardFun stm m = failwith "Not implemented" //stmnt -> Map<int, squareFun> -> boardFun
-
-    // type board = {
-    //     center        : coord
-    //     defaultSquare : squareFun
-    //     squares       : boardFun
-    // }
 
     // let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
     
